dc11b516e07d2cd2614b3c0124971a23845ceba0
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 10:05:53 +0700

    add table driven implementation in KodeProduk

--- KodeProduk.cs
+++ KodeProduk.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace table_driven_and_state_based
 {
-    internal class KodeProduk
+    class KodeProduk
     {
+        public enum Produk
+        {
+            LAPTOP,
+            SMARTPHONE,
+            TABLET,
+            HEADSET,
+            KEYBOARD,
+            MOUSE,
+            PRINTER,
+            MONITOR,
+            SMARTWATCH,
+            CAMERA
+        }
+
+        public static string GetKodeProduk(Produk produk)
+        {
+            string[] kodeProduk = { "E100", "E101", "E102", "E103", "E104", "E105", "E106", "E107", "E108", "E109" };
+
+            return kodeProduk[(int) produk];
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using table_driven_and_state_based;
 
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
+        Console.WriteLine("Kode Produk Camera : " + KodeProduk.GetKodeProduk(KodeProduk.Produk.CAMERA));
+        Console.WriteLine("Kode Produk Headset : " + KodeProduk.GetKodeProduk(KodeProduk.Produk.HEADSET));
+        Console.WriteLine("Kode Produk Monitor : " + KodeProduk.GetKodeProduk(KodeProduk.Produk.MONITOR));
+        Console.WriteLine("Kode Produk Keyboard : " + KodeProduk.GetKodeProduk(KodeProduk.Produk.KEYBOARD));
 
     }
 }
